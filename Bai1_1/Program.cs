df22d47f2de847a42c0ef8a61e8d2bf1e64823da
BaoChau <40660226+baochau122006@users.noreply.github.com>
2022-12-14 13:49:41 +0700

    Add project files.

---
+++ Bai1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap du lieu tu ban phim: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Noi dung vua nhap: {0}", s);
+
+        }
+    }
+}
